de1492404ef9979273bd18a727f6d57a9d7b360e
Sysprog 2017 <sysprog@jessie.vagrantup.com>
2017-10-04 06:43:52 +0000

    Add other scripts

---
+++ bonus_tree.sh
@@ -0,0 +1,19 @@
+echo "Initial directory = $PWD"
+COUNT=0
+
+recursive_traverse() {
+	while [ "$1" ]; do
+		if [ -d "$1" ]; then
+			echo "$1"
+			recursive_traverse "$1"/*
+		fi
+		shift
+	done
+}
+
+recursive_traverse ./* | sed -e "s/[^/]*\// |/g" -e "s/|\([^ ]\)/+---\1/"
+
+for ii in `recursive_traverse ./*`; do
+	COUNT=$(( $COUNT + 1 ))
+done
+echo "Total directories = $COUNT"
---
+++ tree.sh
@@ -0,0 +1,13 @@
+echo "Initial directory = $PWD"
+
+recursive_traverse() {
+	while [ "$1" ]; do
+		if [ -d "$1" ]; then
+			echo "$1"
+			recursive_traverse "$1"/*
+		fi
+		shift
+	done
+}
+
+recursive_traverse ./* | sed -e "s/[^/]*\// |/g" -e "s/|\([^ ]\)/+---\1/"
