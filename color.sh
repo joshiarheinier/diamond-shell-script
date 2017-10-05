10c7829676d9cb5c787c5bcb2cb566208407b474
Joshia Rheinier Pardamean <joshia@joshia-Aspire-V5-471G>
2017-10-05 19:58:14 +0700

    Modify color

--- color.sh
+++ color.sh
@@ -1,1 +1,1 @@
-export PS1="\e[0;31m[\e[m \e[1;33m\u\e[m\e[0;32m@\e[m\e[0;34m\h\e[m \e[1;35m\w\e[m \e[0;31m]\e[m bash-\v\n\e[0;36m\d \@\e[m\n∞>  \e[0;31m"
+export PS1="\e[0;31m[\e[m \e[1;33m\u\e[m\e[0;32m@\e[m\e[0;34m\h\e[m \e[1;35m\w\e[m \e[0;31m]\e[m \e[1;37mbash-\v\e[m\n\e[0;36m\d \@\e[m\n\e[1;37m∞>\e[m  \e[0;31m"
